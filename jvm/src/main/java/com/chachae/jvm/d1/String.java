623a33c490e9c2edf83117033a974739ef933d69
chachae <chenyuexin1998@gmail.com>
2020-03-25 22:41:32 +0800

    feat: 增加 JVM 模块

---
+++ jvm/src/main/java/com/chachae/jvm/d1/String.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+/**
+ * @author chachae
+ * @since 2020/3/25 22:37
+ */
+public class String {
+  public static void main(String[] args) {
+    System.out.println("abc");
+  }
+}
